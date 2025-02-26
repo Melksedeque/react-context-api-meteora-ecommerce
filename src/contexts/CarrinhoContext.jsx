15d91db697ecd9ea70f377d10d641316ae806b44
Melksedeque <freelancer@melksedeque.com.br>
2025-02-26 17:13:09 -0300

    Finalização de context para prover dados para aplicação

--- src/contexts/CarrinhoContext.jsx
+++ src/contexts/CarrinhoContext.jsx
@@ -1,5 +1,4 @@
-import { useReducer } from "react";
-import { useState, createContext, useReducer } from "react";
+import { useState, createContext, useMemo, useReducer, useEffect } from "react";
 import { carrinhoReducer } from "@/reducers/carrinhoReducer";
 
 export const CarrinhoContext = createContext();
@@ -11,6 +10,24 @@ export const CarrinhoProvider = ({ children }) => {
   const [carrinho, dispatch] = useReducer(carrinhoReducer, estadoInicial);
   const [quantidade, setQuantidade] = useState(0);
   const [valorTotal, setValorTotal] = useState(0);
+
+  const { totalTemp, quantidadeTemp } = useMemo(() => {
+    return carrinho.reduce(
+      (contador, produto) => ({
+        quantidadeTemp: contador.quantidadeTemp + produto.quantidade,
+        totalTemp: contador.totalTemp + produto.preco * produto.quantidade,
+      }),
+      {
+        totalTemp: 0,
+        quantidadeTemp: 0,
+      }
+    );
+  }, [carrinho]);
+
+  useEffect(() => {
+    setQuantidade(quantidadeTemp);
+    setValorTotal(totalTemp);
+  });
 
   return (
     <CarrinhoContext.Provider
